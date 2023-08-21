19e423551b79da0688319cbaecd1eb4127f4e5d8
WoodyB-alt <blake-wood@hotmail.com>
2023-08-21 01:21:18 +0100

    Securing the API

--- packages/functions/src/update.js
+++ packages/functions/src/update.js
@@ -2,13 +2,15 @@ import { Table } from "sst/node/table";
 import handler from "@notes/core/handler";
 import dynamoDb from "@notes/core/dynamodb";
 
+
+//make an update DynamoDB call with the new content and attachment values in the params
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
   const params = {
     TableName: Table.Notes.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     Key: {
-      userId: "123", // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: event.pathParameters.id, // The id of the note from the path
     },
     // 'UpdateExpression' defines the attributes to be updated
