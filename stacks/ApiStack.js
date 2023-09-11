f2c0ca213e7de4c5996db22edda616174b27e68c
WoodyB-alt <blake-wood@hotmail.com>
2023-09-11 22:41:04 +0100

    adding a billing API

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -1,16 +1,17 @@
-import { Api, use } from "sst/constructs"
+import { Api, Config, use } from "sst/constructs";
 import  { StorageStack } from "./StorageStack"
 
 
 export function ApiStack({ stack, app  }) {
   const { table } = use(StorageStack);
+  const STRIPE_SECRET_KEY = new Config.Secret(stack, "STRIPE_SECRET_KEY");
   // Create the API
   const api = new Api(stack, "Api", {
     defaults: {
       authorizer: "iam",
-        function: {
-            bind: [table],
-        },
+      function: {
+        bind: [table, STRIPE_SECRET_KEY],
+      },
     },
     routes: {
       "POST /notes": "packages/functions/src/create.main",
@@ -18,6 +19,7 @@ export function ApiStack({ stack, app  }) {
       "GET /notes": "packages/functions/src/list.main",
       "PUT /notes/{id}": "packages/functions/src/update.main",
       "DELETE /notes/{id}": "packages/functions/src/delete.main",
+      "POST /billing": "packages/functions/src/billing.main",
     },
   });
 
